4555cddf6132ba438634b5a5cbe72133461649d3
abundance <abundanceanyanwu@gmail.com>
2025-03-30 10:17:00 +0100

    update:added get products moved richProduct to shared function

--- convex/products.ts
+++ convex/products.ts
@@ -20,63 +20,78 @@ export const getProductImageUrl = query({
   },
 });
 
+async function getRichProduct(ctx: QueryCtx, id: Id<"products">) {
+  const product = await ctx.db.get(id);
+  if (!product)
+    throw new ConvexError({
+      title: "Not Found",
+      message: "No Product Found",
+    });
+
+  // FIXME: move into a shared function
+  async function getCategoryById(
+    parentId: Id<"categories">,
+  ): Promise<DataModel["categories"]["document"][]> {
+    const c = await ctx.db.get(parentId);
+    if (!c) return [];
+    if (!c.parentId) return [c];
+
+    const cat = await getCategoryById(c.parentId);
+    if (!cat) return [c];
+    return [c, ...cat];
+  }
+
+  const categoryTree = await getCategoryById(product.categoryId);
+
+  const richProduct = {
+    ...product,
+    imageUrls: await Promise.all(
+      product.images.map((image) => ctx.storage.getUrl(image)),
+    ),
+    price: product.price,
+    categoryTree,
+    properties: await Promise.all(
+      product.properties.map(async (p) => ({
+        ...p,
+        property: await ctx.db.get(p.propertyId),
+      })),
+    ),
+    variants:
+      product.variants &&
+      (await Promise.all(
+        product.variants.map(async (v) => ({
+          ...v,
+          options: await Promise.all(
+            v.options.map(async (o) => ({
+              ...o,
+              image: o.imageId
+                ? await ctx.storage.getUrl(o.imageId)
+                : undefined,
+            })),
+          ),
+        })),
+      )),
+    comment: null,
+  };
+
+  return richProduct;
+}
+
+export const getProductsById = query({
+  args: {
+    ids: v.array(v.id("products")),
+  },
+  handler: async (ctx, { ids }) => {
+    return Promise.all(ids.map((id) => getRichProduct(ctx, id)));
+  },
+});
+
 export const getProductById = query({
   args: {
     id: v.id("products"),
   },
   handler: async (ctx, { id }) => {
-    const product = await ctx.db.get(id);
-    if (!product)
-      throw new ConvexError({
-        title: "Not Found",
-        message: "No Product Found",
-      });
-
-    // FIXME: move into a shared function
-    async function getCategoryById(
-      parentId: Id<"categories">
-    ): Promise<DataModel["categories"]["document"][]> {
-      const c = await ctx.db.get(parentId);
-      if (!c) return [];
-      if (!c.parentId) return [c];
-
-      const cat = await getCategoryById(c.parentId);
-      if (!cat) return [c];
-      return [c, ...cat];
-    }
-
-    const categoryTree = await getCategoryById(product.categoryId);
-    const richProduct = {
-      ...product,
-      imageUrls: await Promise.all(
-        product.images.map((image) => ctx.storage.getUrl(image))
-      ),
-      price: product.price,
-      categoryTree,
-      properties: await Promise.all(
-        product.properties.map(async (p) => ({
-          ...p,
-          property: await ctx.db.get(p.propertyId),
-        }))
-      ),
-      variants:
-        product.variants &&
-        (await Promise.all(
-          product.variants.map(async (v) => ({
-            ...v,
-            options: await Promise.all(
-              v.options.map(async (o) => ({
-                ...o,
-                image: o.imageId
-                  ? await ctx.storage.getUrl(o.imageId)
-                  : undefined,
-              }))
-            ),
-          }))
-        )),
-      comment: null,
-    };
-
+    const richProduct = await getRichProduct(ctx, id);
     return richProduct;
   },
 });
