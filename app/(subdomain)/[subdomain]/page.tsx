0ba8a40acd42546aed37e13a61670516fa45e4ec
Olalekan Adekanmbi <olamilekanadekanmbi@gmail.com>
2025-03-31 17:51:52 +0100

    Restructure Project Files to support subdomains

---
+++ app/(subdomain)/[subdomain]/page.tsx
@@ -0,0 +1,7 @@
+export default function SubdomainPage() {
+  return (
+    <div className="min-h-svh w-full flex flex-col justify-center items-center">
+      subdomain
+    </div>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ConvexClientProvider>{children}</ConvexClientProvider>
+        {children}
         <Toaster />
       </body>
     </html>
