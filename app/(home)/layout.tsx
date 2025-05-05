6696e1d090b1396aad0fef7c4f912e8f40df833b
Olalekan Adekanmbi <59204288+crazycodestar@users.noreply.github.com>
2025-05-05 20:34:30 +0100

    Implement landing page (#13)
    * feat: Revamp homepage layout and add feature cards for ecommerce platform
    
    * fix: Update header to be sticky with backdrop blur effect

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -9,6 +9,7 @@ import { type Metadata } from "next";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ConvexClientProvider } from "../ConvexClientProvider";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Convertly CMS",
@@ -23,21 +24,32 @@ export default function RootLayout({
   return (
     <ConvexClientProvider>
       <div className="min-h-svh flex flex-col">
-        <header className="flex justify-end items-center p-4 gap-4 h-16">
-          <SignedOut>
-            <SignInButton>
-              <Button>Sign In</Button>
-            </SignInButton>
-            <SignUpButton>
-              <Button variant="outline">Sign Up</Button>
-            </SignUpButton>
-          </SignedOut>
-          <SignedIn>
-            <Button asChild>
-              <Link href={`/dashboard/`}>Dashboard</Link>
-            </Button>
-            <UserButton />
-          </SignedIn>
+        <header className="sticky top-0 z-50 backdrop-blur-lg container px-4 mx-auto flex justify-between items-center gap-4 h-18">
+          <Image src="/logo.svg" alt="logo" width={56} height={56} />
+          <div className="flex items-center gap-4">
+            <SignedOut>
+              <SignInButton>
+                <Button size="sm" className="uppercase text-xs">
+                  Sign In
+                </Button>
+              </SignInButton>
+              <SignUpButton>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="uppercase text-xs"
+                >
+                  Sign Up
+                </Button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <Button asChild size="sm" className="uppercase text-xs">
+                <Link href={`/dashboard/`}>Dashboard</Link>
+              </Button>
+              <UserButton />
+            </SignedIn>
+          </div>
         </header>
         <article className="flex-1 flex flex-col">{children}</article>
       </div>
