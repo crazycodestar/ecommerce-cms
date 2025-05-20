672d3acb795838d25014db9bb3e0c910f105a3a8
abundance <abundanceanyanwu@gmail.com>
2025-05-20 18:48:58 +0100

    implemented deployments

--- packages/backend/convex/actions.ts
+++ packages/backend/convex/actions.ts
@@ -1,6 +1,174 @@
+"use node"
 import { v } from "convex/values";
 import { internal } from "./_generated/api";
 import { internalAction } from "./_generated/server";
+import { Octokit } from "@octokit/rest";
+import { NetlifyAPI } from "@netlify/api";
+
+const GITHUB_ACCESS_TOKEN = process.env.GITHUB_ACCESS_TOKEN;
+const NETLIFY_ACCESS_TOKEN = process.env.NETLIFY_ACCESS_TOKEN;
+
+
+async function downloadImageToBase64(imageUrl: string) {
+  const res = await fetch(imageUrl);
+  if (!res.ok) {
+    throw new Error(`Failed to fetch image: ${res.statusText}`);
+  }
+  const arrayBuffer = await res.arrayBuffer();
+  return Buffer.from(arrayBuffer).toString("base64");
+}
+
+
+
+export async function generateSiteFromContent(
+  storeName: string,
+  storeSlug: string,
+  logoUrl: string,
+) {
+  const octokit = new Octokit({
+    auth: GITHUB_ACCESS_TOKEN,
+  });
+
+
+  const updateGithubFileContents = async (
+    filePath: string,
+    updatedContent: string,
+  ) => {
+    const file = (await octokit.rest.repos.getContent({
+      owner: "convertly-dev",
+      repo: "convertlykit-deployment",
+      path: filePath,
+    })) as any;
+
+    await octokit.rest.repos.createOrUpdateFileContents({
+      owner: "convertly-dev",
+      repo: "convertlykit-deployment",
+      message: "update logo",
+      commiter: {
+        name: "convertly-dev",
+        email: "convertlybusiness@gmail.com",
+      },
+      content: updatedContent,
+      path: filePath,
+      sha: file.data.sha,
+    });
+  }
+
+  const netlify = new NetlifyAPI(NETLIFY_ACCESS_TOKEN);
+  //try creating repo
+  try {
+    await octokit.rest.repos.createUsingTemplate({
+      template_repo: "ecommerce-cms",
+      template_owner: "convertly-dev",
+      name: `${storeSlug}-deployment`,
+    });
+  } catch (error) {
+    //attempt to delete repo if it already exists
+    await octokit.rest.repos.delete({
+      repo: `${storeSlug}-deployment`,
+      owner: "convertly-dev",
+    });
+
+    //try creating repo again
+    await octokit.rest.repos.createUsingTemplate({
+      template_repo: "ecommerce-cms",
+      template_owner: "convertly-dev",
+      name: `${storeSlug}-deployment`,
+    });
+  }
+
+
+  //update logo
+  await updateGithubFileContents(
+    "apps/template/index.html",
+    await downloadImageToBase64(logoUrl),
+  );
+
+  //update store name
+  const storeHtmlData = (await octokit.rest.repos.getContent({
+    owner: "convertly-dev",
+    repo: "convertlykit-deployment",
+    path: "apps/template/index.html",
+  })) as any;
+  const htmlContent = Buffer.from(storeHtmlData.data.content, "base64").toString(
+    "utf-8",
+  );
+
+  const updatedContent = htmlContent.replace("ACME", storeName);
+  await updateGithubFileContents(
+    "apps/template/index.html",
+    Buffer.from(updatedContent, "utf-8").toString("base64"),
+  );
+
+  //create netlify deploy key
+  const deployKey = await netlify.createDeployKey();
+
+  //addd deploy key to github
+  await octokit.rest.repos.createDeployKey({
+    repo: `${storeSlug}-deployment`,
+    owner: "convertly-dev",
+    key: deployKey.public_key!,
+  });
+
+  const site = await netlify.createSite({
+    body: {
+      name: storeSlug,
+      custom_domain: `${storeSlug}.convertlykit.store`,
+      repo: {
+        provider: "github",
+        repo_path: `convertly-dev/${storeSlug}-deployment`,
+        public_repo: false,
+        repo_branch: "main",
+        installation_id: 67440043,
+        deploy_key_id: deployKey.id,
+        cmd: "turbo run build --filter template",
+        dir: "apps/template/dist",
+      },
+      build_settings: {
+        cmd: "turbo run build --filter template",
+        dir: "apps/template/dist",
+      },
+    },
+  });
+
+  await netlify.createEnvVars({
+    site_id: site.id,
+    account_id: site.account_id!,
+    body: [
+      {
+        key: "VITE_CONVEX_URL",
+        values: [
+          {
+            value: process.env.CONVEX_CLOUD_URL!,
+          },
+        ],
+      },
+      {
+        key: "VITE_STORE_SLUG",
+        values: [
+          {
+            value: storeSlug,
+          },
+        ],
+      },
+    ],
+  });
+
+  while (true) {
+    const deployStatus = await netlify.listSiteDeploys({
+      site_id: site.id!,
+    });
+
+    if (deployStatus[0].state == "ready") {
+      break;
+    } else if (["rejected", "error"].includes(deployStatus[0].state!)) {
+      throw new Error("Deploy failed");
+    }
+    setTimeout(() => { }, 1000);
+  }
+
+  return `${storeSlug}.convertlykit.store`;
+}
 
 export const generateSite = internalAction({
   args: {
@@ -20,28 +188,23 @@ export const generateSite = internalAction({
       throw new Error("Store logo not found");
     }
 
-    // TODO: use imageUrl to generate site
-    // const imageUrl = await ctx.storage.getUrl(store.logoId);
+    const imageUrl = await ctx.storage.getUrl(store.logoId);
 
     if (redeploy) {
       // TODO: redeploy site to update logo for instance
       return;
     }
 
-    // TODO: Generate site with logo
-    // const site = await generateSiteFromContent(logo, store.name);
+    const siteUrl = await generateSiteFromContent(store.name, store.slug, imageUrl!);
     // store unique identifier of store
     // const content = JSON.parse(contentJson);
     // TODO: Generate site from content
     // const site = await generateSiteFromContent(content);
 
     // simulate site generation
-    await new Promise((resolve) => setTimeout(resolve, 10000));
-
     await ctx.runMutation(internal.stores.updateStoreByStoreId, {
       storeId,
-      // TODO: replace with the actual generated site url
-      siteUrl: "https://convertlykit.com",
+      siteUrl: siteUrl,
     });
   },
 });
